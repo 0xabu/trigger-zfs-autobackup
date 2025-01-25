576f306f163bd27cf62a1471b60c8b1d4e198fcc
Andrew Baumann <ab@ab.id.au>
2025-01-25 16:52:43 +0100

    don't close the syslog socket when deamonising

--- trigger_zfs_autobackup/app.py
+++ trigger_zfs_autobackup/app.py
@@ -15,7 +15,7 @@ import signal
 import subprocess
 import sys
 import traceback
-from typing import Optional
+from typing import IO, Optional
 
 # Kludge: zfs-autobackup captures sys.argv[0] at import time for use in log output
 _real_argv0 = sys.argv[0]
@@ -37,6 +37,8 @@ class UdevAutobackupMonitor:
     def __init__(self, config: AppConfig):
         self.device_events: queue.Queue[tuple[str, str]] = queue.Queue()
         self.config = config
+        self.udev_monitor = pyudev.Monitor.from_netlink(pyudev.Context())
+        self.udev_monitor.filter_by('block')
 
     def test(self) -> None:
         def is_device_connected(device_label: str) -> bool:
@@ -49,30 +51,18 @@ class UdevAutobackupMonitor:
                 _, msg = import_decrypt_backup_export(pool_config)
                 print(msg)
 
-    def run(self, *, daemon: bool = False) -> None:
-        logger.info(f"Waiting for devices: {', '.join(self.config.pools.keys())}")
-        logger.debug('Using pyudev version: {0}'.format(pyudev.__version__))
-
-        monitor = pyudev.Monitor.from_netlink(pyudev.Context())
-        monitor.filter_by('block')
-
-        if daemon:
-            daemon_ctx = DaemonContext(pidfile=PidFile(PID_FILE_PATH), files_preserve=[monitor])
-            daemon_ctx.open()
-
-        self._wait_for_udev_triggers(monitor)
-
     # Callback for device events, runs in a separate thread
     def _device_callback(self, device: pyudev.Device) -> None:
         fs_type = device.get('ID_FS_TYPE')
         fs_label = device.get('ID_FS_LABEL')
         #fs_uuid = device.get('ID_FS_UUID')
-        if fs_type == "zfs_member" and fs_label and fs_label in self.config.pools and device.action in ("add", "remove"):
+        if fs_type == "zfs_member" and fs_label in self.config.pools and device.action in ("add", "remove"):
             logger.debug(f"udev observed {device.action} of pool {fs_label}")
             self.device_events.put((device.action, fs_label))
 
-    def _wait_for_udev_triggers(self, monitor: pyudev.Monitor) -> None:
-        observer = pyudev.MonitorObserver(monitor, callback=self._device_callback)
+    def run(self) -> None:
+        logger.info(f"Waiting for devices: {', '.join(self.config.pools.keys())}")
+        observer = pyudev.MonitorObserver(self.udev_monitor, callback=self._device_callback)
         observer.start()
         finished_devices: set[str] = set()
 
@@ -222,11 +212,19 @@ def run_command(command: list[str], input: Optional[str] = None) -> subprocess.C
     return result
 
 
-def init_logging(run_as_daemon: bool) -> None:
+def init_logging(run_as_daemon: bool) -> list[IO]:
     # Always log to syslog at INFO level
     syslog_handler = logging.handlers.SysLogHandler(address="/dev/log")
     syslog_handler.ident = APP_NAME + ': '
     syslog_handler.setLevel(logging.INFO)
+
+    # XXX: get socket from SysLogHandler
+    try:
+        sock = syslog_handler.sock # 3.13
+    except AttributeError:
+        sock = syslog_handler.socket # 3.12 and earlier
+    files_preserve = [sock]
+
     logging.basicConfig(
         format='%(levelname)s: %(message)s',
         handlers=(syslog_handler,),
@@ -239,6 +237,8 @@ def init_logging(run_as_daemon: bool) -> None:
         root_logger = logging.getLogger()
         root_logger.addHandler(console_handler)
         root_logger.setLevel(console_handler.level)
+
+    return files_preserve
 
 
 def main() -> None:
@@ -267,9 +267,6 @@ def main() -> None:
     elif args.action != Action.STOP:
         parser.error('config file argument is required')
 
-    run_as_daemon = args.action in {Action.START, Action.RESTART}
-    init_logging(run_as_daemon)
-
     # Stop an existing daemon
     if args.action in {Action.STOP, Action.RESTART}:
         oldpid = None
@@ -287,11 +284,19 @@ def main() -> None:
                 pass
 
     if args.action != Action.STOP:
+        deamonise = args.action in {Action.START, Action.RESTART}
+        files_preserve = init_logging(deamonise)
+
         app = UdevAutobackupMonitor(config)
+        files_preserve.append(app.udev_monitor)
+
         if args.action == Action.TEST:
             app.test()
         else:
-            app.run(daemon=run_as_daemon)
+            if deamonise:
+                daemon_ctx = DaemonContext(pidfile=PidFile(PID_FILE_PATH), files_preserve=files_preserve)
+                daemon_ctx.open()
+            app.run()
 
 
 if __name__ == "__main__":
